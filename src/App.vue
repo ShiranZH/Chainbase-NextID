79b054e7c76183043996ececeb575039e208877c
AngelaEcho <zsr060540@163.com>
2023-02-15 16:58:46 -0500

    Rough demo to get account transactions.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import QueryChainbase from './components/QueryChainbase.vue';
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <a href="https://chainbase.com/" target="_blank">
+      <img src="/chainbase.svg" class="logo chainbase" alt="Chainbase logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://next.id/" target="_blank">
+      <img src="./assets/nextid.svg" class="logo nextid" alt="NextIDs logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <QueryChainbase msg="Chainbase <> NextID" />
+  <!-- <HelloWorld msg="Vite + Vue"/> -->
+  <p>
+    Check out
+    <a href="https://chainbase.com/" target="_blank">chainbase.com</a>,
+    a leading Web3 blockchain interaction layer infrastructure.
+  </p>
+  <p>
+    Check out
+    <a href="https://next.id/" target="_blank">Next.ID</a>,
+    an open-sourced protocol that synergises your Web2 and Web3 profiles.
+  </p>
+  <p class="read-the-docs">Click on the Chainbase and NextID logos to learn more.</p>
+
 </template>
 
 <style scoped>
@@ -21,10 +35,16 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+.read-the-docs {
+    color: #888;
+}
+
 </style>
---
+++ src/components/QueryChainbase.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+    msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<script getChainbaseData>
+import axios from 'axios'
+
+export default {
+    name: "ChainbaseData",
+    data() {
+        return {
+            response: '',
+        }
+    },
+    methods: {
+        getAccountTxs(event) {
+            const options = {
+                url: 'https://api.chainbase.online/v1/account/txs?chain_id=1&address=0xaeaecd497cd167ab0db77356f36eb9a68e888888',
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': '2LWFvlbS8dibH4k7gPEfY4WZ01D',
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            axios(options)
+                .then(response => (this.response = response));
+
+
+            // const response = axios(options);
+            // this.msg = response.data;
+            // alert(`${this.response}`);
+
+        },
+    }
+}
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+    <div id="ChainbaseData">
+        {{ response }}
+    </div>
+
+    <div class="interaction">
+        <!-- <button type="button" @click="count++">count is {{ count }}</button> -->
+        <button @click="getAccountTxs">Get Account Transactions</button>
+    </div>
+
+    <div class="instruction">
+        <p>
+            Click to calculate credit score.
+        </p>
+    </div>
+
+
+</template>
+
